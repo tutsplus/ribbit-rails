6346ad67d93dc488e4b2cf2ecb0bf03adb68792d
Andrew <andrew8088@gmail.com>
2013-03-21 15:07:42 -0400

    changed app name from ribbit to ribbitApp

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ribbit::Application.config.secret_token = '5a33cf935a18f28576200f1b91ea08cb89cdbc5c51f3e81a495e3cae28816bf5d44941d6e053e9430ce747204bab20ea808ae5202b8c168b7913c63fef4e66c3'
+Ribbitapp::Application.config.secret_token = '5a33cf935a18f28576200f1b91ea08cb89cdbc5c51f3e81a495e3cae28816bf5d44941d6e053e9430ce747204bab20ea808ae5202b8c168b7913c63fef4e66c3'
