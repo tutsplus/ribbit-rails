f972ea85794fe2b2f5401f195d5f34c83c70b9f3
Andrew <andrew8088@gmail.com>
2013-03-28 15:01:21 -0400

    added several useful views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Ribbitapp::Application.routes.draw do
   resources :relationships
-
-
   resources :ribbits
-
-
   resources :sessions
   resources :users
 
   get 'logout', to: 'sessions#destroy', as: 'logout' 
+  get 'buddies', to: 'users#buddies', as: 'buddies'
 
   root to: "users#new"
 
