90e78a3e76ccf61addf519b1778f32348193302b
Andrew <andrew8088@gmail.com>
2013-03-29 13:45:24 -0400

    configured which assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,8 @@ module Ribbitapp
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile = ["ribbits.js"]
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
